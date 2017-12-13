814568ab538331b87fee83dcd789ea3452fc9bf4
galperins4 <galperins4@gmail.com>
2017-12-13 10:29:59 -0600

    Create network.py

---
+++ pythark/network.py
@@ -0,0 +1,14 @@
+class Network():
+
+     """
+     Set Network to Use
+     """
+
+     def set_network(self, network):
+     :param: A valid network
+     :return
+   
+     self.network = network
+
+     return self.network
+     
